63cee703eca08620d7e564d3a56ccae72ec7e462
Захаров Павел <pzakharov83@gmail.com>
2025-09-26 15:20:36 +0300

    add a8

--- ADV/HW1/task_A8.c
+++ ADV/HW1/task_A8.c
@@ -1,27 +1,62 @@
 #include <stdint.h>
 #include <stdio.h>
 
-const uint32_t WEIGHTS[] = {1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147, 531441};
+uint32_t WEIGHTS[] = {1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147, 531441};
 
 int main(void) {
   uint64_t input;
   scanf("%lld", &input);
-  if (input > 1000000) {
+  if (input > 797161) { // sum of WEIGHTS
     printf("-1");
     return 0;
+  } else if (input == 797161) {
+    printf("%d", 13);
+    return 0;
   }
-  int count = 0;
-  for (int i = 12; i >= 0; i--) {
-    if (input >= WEIGHTS[i]) {
-      input -= WEIGHTS[i];
-      count++;
+  int current_more_heavy = 0;
+  int current_less_heavy = 0;
+  int index_more_heavy = 0;
+  int index_less_heavy = 0;
+  int cnt = 0;
+  while (input != 0) {
+    for (int i = 0; i < sizeof(WEIGHTS) / sizeof(uint32_t); i++) {
+      if (input <= WEIGHTS[i]) {
+        current_more_heavy = WEIGHTS[i];
+        index_more_heavy = i;
+        break;
+      }
     }
-    if (input <= 0)
+    for (int i = index_more_heavy - 1; i >= 0; i--) {
+      if (WEIGHTS[i] != 0) {
+        index_less_heavy = i;
+        current_less_heavy = WEIGHTS[i];
+        break;
+      }
+    }
+    // if (index_more_heavy == 0 && input == 1) {
+    //   printf("%d", cnt++);
+    //   return 0;
+    // }
+    if (WEIGHTS[index_more_heavy] - input < input - WEIGHTS[index_less_heavy]) {
+      input = WEIGHTS[index_more_heavy] - input;
+      WEIGHTS[index_more_heavy] = 0;
+    } else if (WEIGHTS[index_more_heavy] - input > input - WEIGHTS[index_less_heavy]) {
+      input = input - WEIGHTS[index_less_heavy];
+      WEIGHTS[index_less_heavy] = 0;
+    } else {
+      input = WEIGHTS[index_more_heavy] - input;
+      WEIGHTS[index_more_heavy] = 0;
       break;
+    }
   }
-  if (input < 0)
+
+  for (int i = 0; i < sizeof(WEIGHTS) / sizeof(uint32_t); i++) {
+    if (WEIGHTS[i] == 0)
+      cnt++;
+  }
+  if (input != 0)
     printf("-1");
   else
-    printf("%d", count);
+    printf("%d", cnt);
   return 0;
 }
